58618cfb26e0a26493505f394db2ceaadbc168c0
alimbinyeasin <alimbinyeasin@gmail.com>
2023-11-28 21:15:26 +0600

    Created the first page

--- lib/main.dart
+++ lib/main.dart
@@ -13,19 +13,66 @@ class MyApp extends StatelessWidget {
   }
 }
 
+
 class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue,
       appBar: AppBar(
-        backgroundColor: Colors.amber,
+        backgroundColor: Colors.red,
         title: Text('Book List'),
       ),
       body: ListView(
         padding: EdgeInsets.all(16.0),
         children: [
+          Book('The Lord of the Rings', 'Alim Bin Yeasin', '4.5', Icons.book),
           SizedBox(height: 16.0),
+          Book('The Alchemist', 'Paulo Coelho', '3.8', Icons.book),
+        ],
+      ),
+    );
+  }
+}
+
+
+class Book extends StatelessWidget {
+  final String bookName;
+  final String authorName;
+  final String bookRating;
+  final IconData icon;
+
+  Book(this.bookName, this.authorName, this.bookRating, this.icon);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: [
+          ListTile(
+            leading: CircleAvatar(
+              child: Icon(Icons.book),
+            ),
+            title: Text(bookName),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(authorName),
+                Text('Rating: $bookRating'),
+              ],
+            ),
+          ),
+          ButtonBar(
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+
+                },
+                icon: Icon(icon),
+                label: Text('Buy Now'),
+              ),
+            ],
+          ),
         ],
       ),
     );
